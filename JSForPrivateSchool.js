07e84a3a28c858a9e2227c5c1520182a1125f0d3
danieelbog <54244808+danieelbog@users.noreply.github.com>
2020-01-07 03:22:00 +0200

    Add files via upload

--- JSForPrivateSchool.js
+++ JSForPrivateSchool.js
@@ -4,21 +4,23 @@
 /* Courses Page Section Start */
 
 function addCourses() {
-
-    document.getElementById('POPUP_addCourses').style.display = "block";
+  document.getElementById("POPUP_addCourses").style.display = "block";
+  document.getElementById("maskSectionA").style.display = "block";
 }
 
 function seeCourses() {
-
-    document.getElementById('POPUP_seeCourses').style.display = "block";
+  document.getElementById("POPUP_seeCourses").style.display = "block";
+  document.getElementById("maskSectionB").style.display = "block";
 }
 
 function POPUP_addCourses_QuitSection() {
-    document.getElementById('POPUP_addCourses').style.display = "none";
+  document.getElementById("POPUP_addCourses").style.display = "none";
+  document.getElementById("maskSectionA").style.display = "none";
 }
 
 function POPUP_seeCourses_QuitSection() {
-    document.getElementById('POPUP_seeCourses').style.display = "none";
+  document.getElementById("POPUP_seeCourses").style.display = "none";
+  document.getElementById("maskSectionB").style.display = "none";
 }
 
 /* Courses Page Section Ends */
@@ -26,48 +28,54 @@ function POPUP_seeCourses_QuitSection() {
 /* Students Page Section Start */
 
 function AddStudents() {
-
-    document.getElementById('POPUP_addStudents').style.display = "block";
+  document.getElementById("POPUP_addStudents").style.display = "block";
+  document.getElementById("maskSectionA").style.display = "block";
 }
 
 function SeeStudents() {
-
-    document.getElementById('POPUP_seeStudents').style.display = "block";
+  document.getElementById("POPUP_seeStudents").style.display = "block";
+  document.getElementById("maskSectionB").style.display = "block";
 }
 
 function EnrollStudents() {
-
-    document.getElementById('POPUP_enrollStudents').style.display = "block";
+  document.getElementById("POPUP_enrollStudents").style.display = "block";
+  document.getElementById("maskSectionC").style.display = "block";
 }
 
 function CheckStudentsPerCourse() {
-
-    document.getElementById('POPUP_checkStudentsPerCourse').style.display = "block";
+  document.getElementById("POPUP_checkStudentsPerCourse").style.display =
+    "block";
+  document.getElementById("maskSectionD").style.display = "block";
 }
 
 function CheckStudentsMoreThanOneCourses() {
-
-    document.getElementById('POPUP_checkStudentsMoreThanOneCourses').style.display = "block";
+  document.getElementById("POPUP_checkStudentsMoreThanOneCourses").style.display = "block";
+  document.getElementById("maskSectionE").style.display = "block";
 }
 
 function POPUP_addStudents_QuitSection() {
-    document.getElementById('POPUP_addStudents').style.display = "none";
+  document.getElementById("POPUP_addStudents").style.display = "none";
+  document.getElementById("maskSectionA").style.display = "none";
 }
 
 function POPUP_seeStudents_QuitSection() {
-    document.getElementById('POPUP_seeStudents').style.display = "none";
+  document.getElementById("POPUP_seeStudents").style.display = "none";
+  document.getElementById("maskSectionB").style.display = "none";
 }
 
 function POPUP_enrollStudents_QuitSection() {
-    document.getElementById('POPUP_enrollStudents').style.display = "none";
+  document.getElementById("POPUP_enrollStudents").style.display = "none";
+  document.getElementById("maskSectionC").style.display = "none";
 }
 
 function POPUP_checkStudentsPerCourse_QuitSection() {
-    document.getElementById('POPUP_checkStudentsPerCourse').style.display = "none";
+  document.getElementById("POPUP_checkStudentsPerCourse").style.display = "none";
+  document.getElementById("maskSectionD").style.display = "none";
 }
 
 function POPUP_checkStudentsMoreThanOneCourses_QuitSection() {
-    document.getElementById('POPUP_checkStudentsMoreThanOneCourses').style.display = "none";
+  document.getElementById("POPUP_checkStudentsMoreThanOneCourses").style.display = "none";
+  document.getElementById("maskSectionE").style.display = "none";
 }
 
 /* Students Page Section Ends */
@@ -75,112 +83,121 @@ function POPUP_checkStudentsMoreThanOneCourses_QuitSection() {
 /* Trainers Page Section Start */
 
 function AddTrainer() {
-
-    document.getElementById('POPUP_addTrainers').style.display = "block";
+  document.getElementById("POPUP_addTrainers").style.display = "block";
+  document.getElementById("maskSectionA").style.display = "block";
 }
 
 function SeeTrainers() {
-
-    document.getElementById('POPUP_seeTrainers').style.display = "block";
+  document.getElementById("POPUP_seeTrainers").style.display = "block";
+  document.getElementById("maskSectionB").style.display = "block";
 }
 
 function EnrollTrainers() {
-
-    document.getElementById('POPUP_enrollTrainers').style.display = "block";
+  document.getElementById("POPUP_enrollTrainers").style.display = "block";
+  document.getElementById("maskSectionC").style.display = "block";
 }
 
 function CheckTrainersPerCourse() {
-
-    document.getElementById('POPUP_checkTrainersPerCourse').style.display = "block";
+  document.getElementById("POPUP_checkTrainersPerCourse").style.display = "block";
+  document.getElementById("maskSectionD").style.display = "block";
 }
 
 function PutMarks() {
-
-    document.getElementById('POPUP_putMarks').style.display = "block";
+  document.getElementById("POPUP_putMarks").style.display = "block";
+  document.getElementById("maskSectionE").style.display = "block";
 }
 
-
-
-
 function POPUP_addTrainers_QuitSection() {
-    document.getElementById('POPUP_addTrainers').style.display = "none";
+  document.getElementById("POPUP_addTrainers").style.display = "none";
+  document.getElementById("maskSectionA").style.display = "none";
 }
 
 function POPUP_seeTrainers_QuitSection() {
-    document.getElementById('POPUP_seeTrainers').style.display = "none";
+  document.getElementById("POPUP_seeTrainers").style.display = "none";
+  document.getElementById("maskSectionB").style.display = "none";
 }
 
 function POPUP_enrollTrainers_QuitSection() {
-    document.getElementById('POPUP_enrollTrainers').style.display = "none";
+  document.getElementById("POPUP_enrollTrainers").style.display = "none";
+  document.getElementById("maskSectionC").style.display = "none";
 }
 
 function POPUP_checkTrainersPerCourse_QuitSection() {
-    document.getElementById('POPUP_checkTrainersPerCourse').style.display = "none";
+  document.getElementById("POPUP_checkTrainersPerCourse").style.display =
+    "none";
+  document.getElementById("maskSectionD").style.display = "none";
 }
 
 function POPUP_putMarks_QuitSection() {
-    document.getElementById('POPUP_putMarks').style.display = "none";
+  document.getElementById("POPUP_putMarks").style.display = "none";
+  document.getElementById("maskSectionE").style.display = "none";
 }
-
-
 
 /* Trainers Page Section Ends */
 
 /* Assignments Page Section Starts */
 
 function AddAssignments() {
-
-    document.getElementById('POPUP_addAssignments').style.display = "block";
+  document.getElementById("POPUP_addAssignments").style.display = "block";
+  document.getElementById("maskSectionA").style.display = "block";
 }
 
 function SeeAssignments() {
-
-    document.getElementById('POPUP_seeAssignments').style.display = "block";
+  document.getElementById("POPUP_seeAssignments").style.display = "block";
+  document.getElementById("maskSectionB").style.display = "block";
 }
 
 function EnrollAssignmnets() {
-
-    document.getElementById('POPUP_enrollAssignmnets').style.display = "block";
+  document.getElementById("POPUP_enrollAssignmnets").style.display = "block";
+  document.getElementById("maskSectionC").style.display = "block";
 }
 
 function CheckAssignmnetPerCourse() {
-
-    document.getElementById('POPUP_checkAssignmnetPerCourse').style.display = "block";
+  document.getElementById("POPUP_checkAssignmnetPerCourse").style.display =
+    "block";
+  document.getElementById("maskSectionD").style.display = "block";
 }
 
 function EnrollAssignmentsToStudents() {
-
-    document.getElementById('POPUP_enrollAssignmentsToStudents').style.display = "block";
+  document.getElementById("POPUP_enrollAssignmentsToStudents").style.display = "block";
+  document.getElementById("maskSectionE").style.display = "block";
 }
 
 function CheckAssignmentsPerStudentPerCourse() {
-
-    document.getElementById('POPUP_checkAssignmentsPerStudentPerCourse').style.display = "block";
+  document.getElementById(
+    "POPUP_checkAssignmentsPerStudentPerCourse").style.display = "block";
+  document.getElementById("maskSectionF").style.display = "block";
 }
 
-
 function POPUP_addAssignments_QuitSection() {
-    document.getElementById('POPUP_addAssignments').style.display = "none";
+  document.getElementById("POPUP_addAssignments").style.display = "none";
+  document.getElementById("maskSectionA").style.display = "none";
 }
 
 function POPUP_seeAssignments_QuitSection() {
-    document.getElementById('POPUP_seeAssignments').style.display = "none";
+  document.getElementById("POPUP_seeAssignments").style.display = "none";
+  document.getElementById("maskSectionB").style.display = "none";
 }
 
 function POPUP_enrollAssignmnets_QuitSection() {
-    document.getElementById('POPUP_enrollAssignmnets').style.display = "none";
+  document.getElementById("POPUP_enrollAssignmnets").style.display = "none";
+  document.getElementById("maskSectionC").style.display = "none";
 }
 
 function POPUP_checkAssignmnetPerCourse_QuitSection() {
-    document.getElementById('POPUP_checkAssignmnetPerCourse').style.display = "none";
+  document.getElementById("POPUP_checkAssignmnetPerCourse").style.display =
+    "none";
+  document.getElementById("maskSectionD").style.display = "none";
 }
 
 function POPUP_enrollAssignmentsToStudents_QuitSection() {
-    document.getElementById('POPUP_enrollAssignmentsToStudents').style.display = "none";
+  document.getElementById("POPUP_enrollAssignmentsToStudents").style.display = "none";
+  document.getElementById("maskSectionE").style.display = "none";
 }
 
 function POPUP_checkAssignmentsPerStudentPerCourse_QuitSection() {
-    document.getElementById('POPUP_checkAssignmentsPerStudentPerCourse').style.display = "none";
+  document.getElementById("POPUP_checkAssignmentsPerStudentPerCourse").style.display = "none";
+  document.getElementById("maskSectionF").style.display = "none";
 }
 
 /* Assignments Page Section Ends */
